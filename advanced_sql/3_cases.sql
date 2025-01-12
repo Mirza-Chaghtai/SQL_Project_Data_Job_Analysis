8fd229ffe24f5c64fbf7f9753cf6ffeaa591c412
Mirza Arbaz Chaghtai <machaghtai@gmail.com>
2025-01-12 22:08:05 +0000

    first commit

---
+++ advanced_sql/3_cases.sql
@@ -0,0 +1,57 @@
+-- January
+CREATE TABLE january_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1;
+
+-- February
+CREATE TABLE february_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 2;
+
+-- March
+CREATE TABLE march_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 3;
+
+SELECT job_posted_date
+FROM january_jobs;
+
+SELECT job_posted_date
+FROM february_jobs;
+
+SELECT job_posted_date
+FROM march_jobs; 
+
+/*
+Lable new colum as follows:
+-Anywhere jobs as remote
+-new york ny jobs as local
+-otherwise onsite
+*/
+
+SELECT
+    job_title_short,
+    job_location,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM job_postings_fact;
+
+SELECT
+    COUNT(job_id) AS number_of_jobs,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM 
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY 
+    location_category;
